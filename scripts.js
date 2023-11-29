0275255d82754d3143102dce0ee816af10ab30d8
FacuHdezz <facuhernandez2000@gmail.com>
2023-11-29 19:15:41 -0300

    culminando con portfolio, arreglando responsive y añadiendo tema claro

--- scripts.js
+++ scripts.js
@@ -1,10 +1,44 @@
-let abrir = document.getElementById("menu");
-let cerrar = document.getElementById("close");
+let btnLight = document.getElementsByClassName("btn-light");
+let btnDark = document.getElementsByClassName("btn-dark");
 
-abrir.addEventListener("click", () => {
-    document.getElementById("mobile-menu").style.width = "100%";
+btnLight[0].addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', 'dark');
 });
 
-cerrar.addEventListener("click", () => {
-    document.getElementById("mobile-menu").style.width = "0%";
+btnDark[0].addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', 'light');
 });
+
+function getTime(){
+    let fecha = new Date();
+    let horas = fecha.getHours();
+    let minutos = fecha.getMinutes();
+    let segundos = fecha.getSeconds();
+
+    let diaSemana = obtenerDiaSemana(fecha.getDay());
+    let dia = fecha.getDate();
+    let mes = obtenerNombreMes(fecha.getMonth());
+    let año = fecha.getFullYear();
+
+    horas = horas < 10 ? '0' + horas : horas;
+    minutos = minutos < 10 ? '0' + minutos : minutos;
+    segundos = segundos < 10 ? '0' + segundos : segundos;
+
+    document.getElementById('dateAsset').innerHTML = horas + ':' + minutos + ':' + segundos;
+    document.getElementById('dia-semana').innerHTML = `${diaSemana}`;
+    document.getElementById('fecha').innerHTML = `${dia} de ${mes} de ${año}`;
+}
+
+function obtenerDiaSemana(numeroDia) {
+    let diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    return diasSemana[numeroDia];
+}
+
+function obtenerNombreMes(numeroMes) {
+    let meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    return meses[numeroMes];
+}
+
+setInterval(getTime, 1000);
+
+getTime();
